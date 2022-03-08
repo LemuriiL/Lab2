2e4ac147d307ea501151b7e15b6a165a433de3ba
Mamongo Mr <mrmamongo@gmail.com>
2022-03-08 22:02:00 +0300

    add theory

--- sources/main.cpp
+++ sources/main.cpp
@@ -4,7 +4,42 @@
 
 #include <iostream>
 
-int main() {
-  std::cout << "Hallo!";
+enum Colours
+{
+  COLOUR_GRAY,
+  COLOUR_PINK,
+  COLOUR_BLUE,
+  COLOUR_PURPLE,
+  COLOUR_RED
+};
+
+void printCOLOUR(Colours colour)
+{
+  std::cout << "The colour is: ";
+  switch (colour) {
+    case COLOUR_GRAY:
+      std::cout << "gray";
+      break;
+    case COLOUR_PINK:
+      std::cout << "pink";
+    case COLOUR_BLUE:
+      std::cout << "blue";
+      break;
+    case COLOUR_PURPLE:
+      std::cout << "purple";
+      break;
+    case COLOUR_RED:
+      std::cout << "red";
+      break;
+    default:
+      std::cout << "unknown";
+      break;
+  }
+  std::cout << "\n";
+}
+
+int main()
+{
+  printCOLOUR(COLOUR_BLUE);
   return 0;
 }
